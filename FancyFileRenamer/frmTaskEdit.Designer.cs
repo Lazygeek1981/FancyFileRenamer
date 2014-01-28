7189230152253c221a3602b62fdbc5bac8c5ed63
Christian Zimmermann <christian.zimmermann@leue.de>
2014-01-28 12:31:33 +0100

    Started TaskEditForm

---
+++ FancyFileRenamer/frmTaskEdit.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace FancyFileRenamer
+{
+  partial class frmTaskEdit
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.btnAbort = new System.Windows.Forms.Button();
+      this.btnOk = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+      this.splitContainer1.Name = "splitContainer1";
+      this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.btnOk);
+      this.splitContainer1.Panel2.Controls.Add(this.btnAbort);
+      this.splitContainer1.Size = new System.Drawing.Size(415, 382);
+      this.splitContainer1.SplitterDistance = 319;
+      this.splitContainer1.TabIndex = 0;
+      // 
+      // btnAbort
+      // 
+      this.btnAbort.Location = new System.Drawing.Point(328, 19);
+      this.btnAbort.Name = "btnAbort";
+      this.btnAbort.Size = new System.Drawing.Size(75, 23);
+      this.btnAbort.TabIndex = 0;
+      this.btnAbort.Text = "Cancel";
+      this.btnAbort.UseVisualStyleBackColor = true;
+      this.btnAbort.Click += new System.EventHandler(this.btnAbort_Click);
+      // 
+      // btnOk
+      // 
+      this.btnOk.Location = new System.Drawing.Point(247, 19);
+      this.btnOk.Name = "btnOk";
+      this.btnOk.Size = new System.Drawing.Size(75, 23);
+      this.btnOk.TabIndex = 1;
+      this.btnOk.Text = "OK";
+      this.btnOk.UseVisualStyleBackColor = true;
+      this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+      // 
+      // frmTaskEdit
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(415, 382);
+      this.Controls.Add(this.splitContainer1);
+      this.Name = "frmTaskEdit";
+      this.Text = "Edit Task";
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private System.Windows.Forms.Button btnOk;
+    private System.Windows.Forms.Button btnAbort;
+  }
+}
---
+++ FancyFileRenamer/frmTaskEdit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FancyFileRenamer
+{
+  public partial class frmTaskEdit : Form
+  {
+    private ITaskEditControl taskEditControl = null;
+
+    public frmTaskEdit()
+    {
+      InitializeComponent();
+    }
+
+    public void SetTaskEditControl(ITaskEditControl control)
+    {
+      this.taskEditControl = control;
+
+      splitContainer1.Panel1.Controls.Clear();
+      splitContainer1.Panel1.Controls.Add(taskEditControl as Control);
+    }
+
+    private void btnOk_Click(object sender, EventArgs e)
+    {
+      taskEditControl.UpdateTaskFromControl();
+      DialogResult = System.Windows.Forms.DialogResult.OK;
+      this.Hide();
+    }
+
+    private void btnAbort_Click(object sender, EventArgs e)
+    {
+      DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.Hide();
+    }
+  }
+}
