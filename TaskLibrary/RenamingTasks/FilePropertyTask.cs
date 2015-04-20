fec16a84d722254bffb6eeb81170429dd50cd9f1
Chris <christian.zimmermann1981@gmail.com>
2015-04-20 13:18:02 +0200

    Added FilePropertyTask

---
+++ TaskLibrary/RenamingTasks/FilePropertyTask.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyFileRenamer.TaskLibrary.RenamingTasks
+{
+	public class FilePropertyTask : AbstractTask, IRenamingTask
+	{
+		#region IRenamingTask Members
+
+		public void ApplyOn(File datei)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IRenamingTask Self
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public IRenamingTask GetNewInstance()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ResetTask()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+		public override string NameInTaskSelectionList
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override string Description
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
